174813f037ce9351082b38b336aa5aab2571745c
Anthony Surkov <anthonysurkov@gmail.com>
2026-08-05 15:58:26 -0700

    moved baseline out of src

--- src/baseline/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Position-aware k-mer regression of editing rate (interpretable baseline)."""
-
-from .featurize import build_design, encode, feature_name, feature_names
-from .regression import KmerRegression, fit_kmer_regression
-
-__all__ = [
-    "encode", "build_design", "feature_name", "feature_names",
-    "KmerRegression", "fit_kmer_regression",
-]
